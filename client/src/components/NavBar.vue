022d7d7b007a730228713a53bdf5dfe4036647f2
Renato Avellar Nobre <rekanobre@gmail.com>
2019-12-13 12:42:59 -0300

    Thank god never audit this thing again

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <b-navbar toggleable="lg" type="dark" variant="info" sticky=true >
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">About Me</b-nav-item>
+          <b-nav-item href="#">Skills</b-nav-item>
+          <b-nav-item href="#">Portifolio</b-nav-item>
+          <b-nav-item href="#">Certificates</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">PT</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
