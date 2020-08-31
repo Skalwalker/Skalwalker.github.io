a066cbfc641224ce8e6c566038a18e81d84bc236
Renato Avellar Nobre <rekanobre@gmail.com>
2020-08-31 14:17:46 -0300

    Updates

---
+++ precache-manifest.227469dd1decc639e484e7be2a37a515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb660589b60d148aa03294ea43718ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2143b6c13908a2a995b",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "3e73e66dea78eb2c80b0",
+    "url": "/static/css/main.2e11f28f.chunk.css"
+  },
+  {
+    "revision": "c2143b6c13908a2a995b",
+    "url": "/static/js/2.d6f9af6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d6f9af6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e73e66dea78eb2c80b0",
+    "url": "/static/js/main.61082fb6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fe8eb666df8a7b119403c64bfd64300.js"
+  "/precache-manifest.227469dd1decc639e484e7be2a37a515.js"
 );
 
 self.addEventListener('message', (event) => {
