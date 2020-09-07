568f91e2a518825822518d8542d47b068142db69
khalil09 <khalilcarsten@gmail.com>
2020-09-07 13:30:30 -0300

    canvas e experince view

--- src/views/aboutViews/Likes.tsx
+++ src/views/aboutViews/Likes.tsx
@@ -3,19 +3,20 @@ import { Nav, Container, Row, Col } from 'react-bootstrap';
 import Popover from 'react-tiny-popover'
 
 type MyProps = { };
-type MyState = { isPopoverOpen: boolean };
+type MyState = { isShown: boolean };
 
 class Likes extends React.Component<MyProps, MyState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      isPopoverOpen: false
+      isShown: false
     };
   }
 
-  setIsPopoverOpen = (value: any) => {
-    this.setState({ isPopoverOpen: value})
+  setIsShown = (value: boolean) => {
+    this.setState({isShown:value})
   }
+
 
   render() {
     const squareRows = [];
@@ -23,15 +24,6 @@ class Likes extends React.Component<MyProps, MyState> {
 
     for (var i = 0; i < numbRows; i++) {
       squareRows.push(<Row className='mb-5'>
-        <Col md={1} className='mr-5 ml-5'>
-          <Popover
-            isOpen={this.state.isPopoverOpen}
-            position={'top'} // preferred position
-            content={<div>Hi! I'm popover content.</div>}
-          >
-            <div onMouseEnter={() => this.setIsPopoverOpen(1)} className='m-auto' style={{height: '70px', width: '70px', backgroundColor: 'gray'}}></div>
-          </Popover>
-        </Col>
         <Col md={1} className='mr-5'>
           <div className='m-auto' style={{height: '70px', width: '70px', backgroundColor: 'gray'}}></div>
         </Col>
